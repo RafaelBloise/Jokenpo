fc4a99b4e3c58ef9fd04376b24433ccc9da76968
RafaelBloise <rafael.c.bloise@outlook.com>
2019-06-13 17:33:08 -0300

    Alterações

--- app/src/main/java/com/example/jokenpo/MainActivity.java
+++ app/src/main/java/com/example/jokenpo/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.jokenpo;
 
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageViewPlayer, imageViewAdversario;
+    private ImageView imageViewPlayer, imageViewAdversario, imageViewResultado;
     private ImageButton buttonPedra, buttonPapel, buttonTesoura;
     private int jogadaUsuario, jogadaAdversario;
+    private MediaPlayer musica;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
         imageViewPlayer = findViewById(R.id.player);
         imageViewAdversario = findViewById(R.id.advesario);
+        imageViewResultado = findViewById(R.id.imageViewResultado);
+        frameLayout = findViewById(R.id.frameResultado);
 
         buttonPapel = findViewById(R.id.papel);
         buttonPedra = findViewById(R.id.pedra);
         buttonTesoura = findViewById(R.id.tesoura);
+
 
         buttonPapel.setOnClickListener(new OnClickListener() {
             @Override
@@ -76,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        this.musica.pause();
     }
 
     @Override
@@ -89,20 +100,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void jogadaUsuario(int i) {
+
         switch (i) {
             case 1:
                 imageViewPlayer.setImageResource(R.drawable.papel);
-                Toast.makeText(getApplicationContext(), "Jogou Papel", Toast.LENGTH_LONG).show();
                 break;
 
             case 2:
                 imageViewPlayer.setImageResource(R.drawable.pedra);
-                Toast.makeText(getApplicationContext(), "Jogou Pedra", Toast.LENGTH_LONG).show();
                 break;
 
             case 3:
                 imageViewPlayer.setImageResource(R.drawable.tesoura);
-                Toast.makeText(getApplicationContext(), "Jogou Tesoura", Toast.LENGTH_LONG).show();
                 break;
         }
         jogadaMaquina(jogadaAdversario);
@@ -127,21 +136,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void resultado() {
-
+        final int TEMPO = 2500;
         if (jogadaUsuario != jogadaAdversario) {
             if ((jogadaUsuario == 1 && jogadaAdversario == 2) || (jogadaUsuario == 2 && jogadaAdversario == 3) || (jogadaUsuario == 3 && jogadaAdversario == 1)) {
                 Toast.makeText(getApplicationContext(), "Mijou grosso! Ganhou!", Toast.LENGTH_LONG).show();
+                this.musica = MediaPlayer.create(this, R.raw.bodybuilderporra);
+                musica.start();
+                frameLayout.setVisibility(View.VISIBLE);
+                Glide.with(this)
+                        .load(R.raw.bambamgif)
+                        .fitCenter()
+                        .into(imageViewResultado);
+
+
             } else {
                 Toast.makeText(getApplicationContext(), "Levou mijada! Perdeu!", Toast.LENGTH_LONG).show();
+                this.musica = MediaPlayer.create(this, R.raw.gemidao);
+                musica.start();
             }
         } else {
             Toast.makeText(getApplicationContext(), "Incompentente...deu empate!", Toast.LENGTH_LONG).show();
+            this.musica = MediaPlayer.create(this, R.raw.alex_play);
+            musica.start();
         }
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                frameLayout.setVisibility(View.GONE);
+                Toast.makeText(getApplicationContext(), "NOVA RODADA!", Toast.LENGTH_SHORT).show();
+                reiniciar();
+            }
+        }, TEMPO);
+
+
     }
 
-    public void reiniciar(){
-
+    public void reiniciar() {
+        imageViewPlayer.setImageResource(R.drawable.interrogacao);
+        imageViewAdversario.setImageResource(R.drawable.interrogacao);
     }
+
 
 }
